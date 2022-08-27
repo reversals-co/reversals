553a05f3a401556311038fd1fd22ecafc125c382
Hamza Saqib <mianhamza7262@gmail.com>
2022-08-27 17:36:51 +0500

    meta tag noindex added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta name="robots" content="noindex, nofollow">
   <meta name="description" content="Mintech  IT Solutions &  Services Template">
   <link href="{{asset('assets/website')}}/images/favicon/favicon.png" rel="icon">
   <title>Mintech IT Solutions & Services Template</title>
